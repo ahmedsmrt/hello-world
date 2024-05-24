cc5329513a7dc0cc21a38a514c9d1f0f05f0bbef
agasimdev <agasim@archeredu.com>
2024-05-23 21:39:53 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class HelloWorld 
+{
+    public static void Main(string[] args)
+    {
+        string paragraph = "What's good out thurr"; 
+
+        int number = 2;
+        float price = 3.92f;
+
+        Console.WriteLine(paragraph);
+
+        Console.WriteLine(number * price);
+    }
+}
