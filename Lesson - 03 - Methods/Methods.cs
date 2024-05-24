0ea14503d304bec0a23a2f43fb52636c262948f9
agasimdev <agasim@archeredu.com>
2024-05-23 23:00:10 -0400

    dev: created two methods that write text and accept arguments from cli

---
+++ Lesson - 03 - Methods/Methods.cs
@@ -0,0 +1,26 @@
+// using System;
+
+// namespace HelloWorld
+// {
+//     class Program 
+//     {
+//         static void Main(string[] args)
+//         {
+//             WriteSomething();
+//             WriteSomethingSpecific(args[0]);
+//             Console.Read(); // Waits for Enter key press before closing
+//         }
+
+//         // Access modifier (static) return type method name (param 1, param2)
+//         // Methods should only do one thing!
+//         public static void WriteSomething()
+//         {
+//             Console.WriteLine("I am called a method");
+//         }
+
+//         public static void WriteSomethingSpecific(string myText)
+//         {
+//             Console.WriteLine(myText);
+//         }
+//     }
+// }
