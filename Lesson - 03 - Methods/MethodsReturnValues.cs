484582b9a84995d0eb03376acb49dba8dd246601
agasimdev <agasim@archeredu.com>
2024-05-23 23:35:38 -0400

    dev: the worlds most robust calculator app

---
+++ Lesson - 03 - Methods/MethodsReturnValues.cs
@@ -0,0 +1,30 @@
+// using System;
+
+// namespace HelloWorld
+// {
+//     class Program 
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine(Add(Add(15, 32), Add(15, 32)));
+//             Console.WriteLine(Multiply(Multiply(15, 32), Multiply(15, 32)));
+//             Console.WriteLine(Divide(498, 35));
+//             Console.ReadLine();
+//         }
+
+//         public static int Add(int num1, int num2)
+//         {
+//             return num1 + num2;
+//         }
+
+//         public static int Multiply(int num1, int num2)
+//         {
+//             return num1 * num2;
+//         }
+
+//         public static double Divide(double num1, double num2)
+//         {
+//             return num1 / num2;
+//         }        
+//     }
+// }
