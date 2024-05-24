55688399a73e526778d881b189b1eba1bc5e59e4
agasimdev <agasim@archeredu.com>
2024-05-23 22:05:33 -0400

    dev: updated code to mention primitive data types

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 
 public class HelloWorld 
 {
-    static float vat = 7.8f;
+    // Primitive Data Types
+    float price = 7.98f;
+    string name = "Ahki";
+    char letter = 'Z';
+    int number = 100000;
+    bool toggle = true;
 
     public static void Main(string[] args)
     {
-        float vat = 17.8f;
         Console.WriteLine("Total vat is: " + vat);
     }
 }
