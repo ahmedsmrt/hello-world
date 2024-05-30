7a20948a10261ef562eb3908897cb42ca62199f7
agasimdev <agasim@archeredu.com>
2024-05-30 19:16:34 -0400

    dev: worked through different approaches to getting and setting data within a class using methods and property shorthands

---
+++ Lesson - 07 - Classes/ClassesBasics.cs
@@ -0,0 +1,19 @@
+// using System;
+
+// namespace ClassesBasics
+// {
+//     class Program 
+//     {
+//         static void Main(string[] args)
+//         {               
+//            //How to create a new class
+//            Human ahmed = new Human("Ahmed", "Gasim", "Green", 32);
+//            Human zacharia = new Human("Zacharia", "Withers", "Black", 42);
+//            //Access public variable from external class
+//            ahmed.IntroduceMyself();
+//            zacharia.IntroduceMyself();
+
+//            Console.ReadKey(true);
+//         }
+//     }
+// }
---
+++ Properties.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Properties
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {               
+            Box box = new Box();
+            // box.length = 5;
+            box.SetLength(8);
+            // box.width = 6;
+            box.Width = 12; 
+            box.Height = 11;
+            Console.WriteLine($"{box.GetLength()}");
+            box.DisplayInfo();
+        }
+    }
+}
