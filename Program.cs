364a8c4deda601df6e0765c461bee690f13ce227
agasimdev <agasim@archeredu.com>
2024-06-03 15:31:27 -0400

    dev: went through lesson for understanding the basics of Arrays

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
 using System;
 
-namespace MembersC
+namespace ArraysC
 {
     class Program 
     {
         static void Main(string[] args)
-        {               
-            Members member1 = new Members();
-            member1.Introducing(false);
-        }
+        {
+                    
+        int[] grades = new int[5];
 
+        grades[0] = 5;
+        grades[1] = 12;
+        grades[2] = 16;
+        grades[3] = 20;
+        grades[4] = 25;      
+      
+        int[] gradesOfMathStudentsA = {98, 89, 79, 54, 12};
+
+        Console.WriteLine($"This is the array for ints {grades.Length}!");
+        System.Console.WriteLine($"These are the grades for our math strudents {gradesOfMathStudentsA.Length};");   
+        }
     }
 }
