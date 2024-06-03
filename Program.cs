4d4e06e16a33caffd67f8812da37eca7f7d8e289
agasimdev <agasim@archeredu.com>
2024-06-03 11:20:58 -0400

    dev: completed lesson to understand Members in C#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MembersC
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {               
+            Members member1 = new Members();
+            member1.Introducing(false);
+        }
+
+    }
+}
