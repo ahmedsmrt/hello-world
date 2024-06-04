ef182b0f029e8358bdcb9deca84b9e970b8940a4
agasimdev <agasim@archeredu.com>
2024-06-04 10:39:44 -0400

    dev: worked with nested for loops and 2d arrays

--- Program.cs
+++ Program.cs
@@ -4,40 +4,31 @@ namespace Program
 {
     class Program 
     {
+
+        static int[,] matrix = 
+        {
+            {1,2,3}, // row 0
+            {4,5,6}, // row 1
+            {7,8,9}  // row 2
+        };
+
+
         static void Main(string[] args)
         {
-            //  Declare 2d array
-            string[,] matrix;
-
-            // Declare 3d array
-            int[,,] threeD;
-
-            int[,] array2d = new int[,]
+            foreach (int item in matrix)
             {
-                {1,2,3}, // row 0
-                {4,5,6}, // row 1
-                {7,8,9}  // row 2
-            };
+                System.Console.Write(item + " ");
+            }
 
+            System.Console.WriteLine("\nThis is our 2d array being printed using a nested for loop.");
 
-            string[,] array2dString = new string[3,2]
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                {"one", "two"}, // row 0
-                {"three", "four"}, // row 1
-                {"five", "six"}  // row 2
-            };            
-
-            array2dString[2,1] = "turkey";
-
-            int dimensions = array2dString.Rank;
-
-            System.Console.WriteLine("Central value is {0}", array2d[2,0]);
-
-            System.Console.WriteLine("Central value is {0} and the rank is {1}", array2dString[2,1], dimensions);
-
-
-
-
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    System.Console.Write(matrix[i,j] + " ");
+                }
+            }
         }
     }
 }
