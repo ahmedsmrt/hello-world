7987b6a314120b147b7b915d35cdb9271d9cfe72
agasimdev <agasim@archeredu.com>
2024-06-04 10:22:19 -0400

    dev: finished array lessons

--- Program.cs
+++ Program.cs
@@ -6,20 +6,38 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            // Define the size of the array
-            int size = 5;
-            
-            // Initialize the array
-            string[] friendList = new string[size];
+            //  Declare 2d array
+            string[,] matrix;
 
-            // Prompt the user to input names and greet each friend
-            for (int i = 0; i < size; i++)
+            // Declare 3d array
+            int[,,] threeD;
+
+            int[,] array2d = new int[,]
             {
-                Console.Write("Please put in your friend's name: ");
-                string friend = Console.ReadLine();
-                friendList[i] = friend;
-                Console.WriteLine($"Hey {friend}, how've you been fam!");
-            }
+                {1,2,3}, // row 0
+                {4,5,6}, // row 1
+                {7,8,9}  // row 2
+            };
+
+
+            string[,] array2dString = new string[3,2]
+            {
+                {"one", "two"}, // row 0
+                {"three", "four"}, // row 1
+                {"five", "six"}  // row 2
+            };            
+
+            array2dString[2,1] = "turkey";
+
+            int dimensions = array2dString.Rank;
+
+            System.Console.WriteLine("Central value is {0}", array2d[2,0]);
+
+            System.Console.WriteLine("Central value is {0} and the rank is {1}", array2dString[2,1], dimensions);
+
+
+
+
         }
     }
 }
